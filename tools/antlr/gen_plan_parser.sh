e3cb649d0fe83e74ee906246580cfb2460354839
xaxys <tpnnghd@163.com>
2021-11-03 23:36:23 +0800

    Modified plan_parser generation script
    Signed-off-by: xaxys <tpnnghd@163.com>

--- tools/antlr/gen_plan_parser.sh
+++ tools/antlr/gen_plan_parser.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [ ! -f "/usr/local/lib/antlr-4.9-complete.jar" ];then
-    echo "downloading antlr-4.9"
-    curl -o /usr/local/lib/antlr-4.9-complete.jar https://www.antlr.org/download/antlr-4.9-complete.jar
-    echo "download complete"
+if [ ! -f "/tmp/antlr-4.9-complete.jar" ];then
+    curl -o "/tmp/antlr-4.9-complete.jar" https://www.antlr.org/download/antlr-4.9-complete.jar
 fi
 
-java -Xmx500M -cp "/usr/local/lib/antlr-4.9-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Go -visitor -o . ../../internal/proxy/plan_parser/Plan.g4
+java -Xmx500M -cp "/tmp/antlr-4.9-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Go -visitor -o . ../../internal/proxy/plan_parser/Plan.g4
